74831087cce6605b0594200d481889d3ce1881bf
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-07-01 12:11:34 +0300

    done

--- server/models.py
+++ server/models.py
@@ -11,4 +11,12 @@ db = SQLAlchemy(metadata=metadata)
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
+    
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+     
+    def __repr__(self):
+        return f'<Message by {self.username}: {self.body[:10]}...>'
